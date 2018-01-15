a592f6035250a6965ce6d5c9a546742b0ff8208d
Max Isom <codetheweb@icloud.com>
2018-01-15 11:40:05 -0600

    Finish cleanup

--- inits.py
+++ inits.py
@@ -14,23 +14,3 @@ class Component(object):
         self.driveTrain = {'frontLeft': wpilib.Spark(Mapping.frontLeftM), 'backLeft': wpilib.Spark(Mapping.backLeftM), 'frontRight': wpilib.Spark(Mapping.frontRightM), 'backRight': wpilib.Spark(Mapping.backRightM)}
         self.driveTrain['frontLeft'].setInverted(True)
         self.driveTrain['backLeft'].setInverted(True)
-
-        # Init other motors
-        self.shooterM    = wpilib.Spark(Mapping.shooterM)
-        self.hopperM     = wpilib.Spark(Mapping.hopperM)
-        self.climbM      = wpilib.Spark(Mapping.climbM)
-        self.groundGearM = wpilib.Talon(Mapping.groundGearM)
-
-        # Init soleniods
-        self.gearSol    = wpilib.DoubleSolenoid(Mapping.gearSol['out'], Mapping.gearSol['in'])
-        self.groundSol  = wpilib.DoubleSolenoid(Mapping.groundGearSol['out'], Mapping.groundGearSol['in'])
-
-        # Init sensors
-        self.gyroS      = wpilib.ADXRS450_Gyro(Mapping.gyroS)
-        self.allienceS  = wpilib.DigitalInput(Mapping.allienceS)
-        self.shooterS   = wpilib.DigitalInput(Mapping.shooterS)
-        self.hopperS    = wpilib.DigitalInput(Mapping.hopperS)
-
-        # Init relays
-        self.bumpPopR   = wpilib.Relay(Mapping.bumpPopR)
-        self.greenLEDR  = wpilib.Relay(Mapping.greenLEDR)
--- map.py
+++ map.py
@@ -9,23 +9,3 @@ class Map(object):
         self.frontRightM = 3
         self.backLeftM   = 0
         self.backRightM  = 1
-
-        self.climbM      = 7
-        self.collectorM  = 5
-        self.shooterM    = 9
-        self.hopperM     = 8
-        self.groundGearM = 4
-
-        # Soleniods
-        self.gearSol     = {'in': 3, 'out': 4}
-        self.groundGearSol = {'in': 2, 'out': 0}
-
-        # Sensors have suffix 'S'. Gyro uses SPI, everything else uses the DIO.
-        self.gyroS       = 0
-        self.allienceS   = 0
-        self.shooterS    = 1
-        self.hopperS     = 2
-
-        # Relays
-        self.bumpPopR    = 0
-        self.greenLEDR   = 1
